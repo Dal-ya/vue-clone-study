3d0099fc8182f56cdf1203b5fc88ee5737e3e30a
Dal-ya <hyansky@daum.net>
2022-03-26 14:02:44 +0900

    fix | tw 문제로 삭제

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div
+      class="container mx-auto box-border border-dashed border-2 border-purple-500"
+    >
+      <h1 class="text-center font-bold underline text-blue-500">
+        This is an about page
+      </h1>
+      <div>card</div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-@import '../styles/About';
+//@import '../styles/About';
 </style>
---
+++ src/views/GreenPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const msg = ref('hello world');
+</script>
+
+<style scoped></style>
